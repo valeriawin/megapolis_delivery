8386cfe4b17e7363b1f8b27b53da814d918fde42
valeriawin <baranova.valeriya.by@gmail.com>
2022-07-24 22:06:09 +0300

    docs: add docstring to deliver function

--- routers/deliver.py
+++ routers/deliver.py
@@ -10,6 +10,27 @@ delivery_cache = {}
 
 @router.post("/deliver", response_model=Feature)
 async def add_zone(request: Request) -> Feature:
+    """ Args:
+            request: body should contain a geojson feature information as JSON
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [13.38272, 52.46385]
+                        },
+                        "properties": {
+                            "order": "iPhone 13 256gb"
+                        }
+                    }
+
+        Returns:
+            Delivery zone Feature
+
+        Raises:
+            HTTPException 400: If nothing passed
+            HTTPException 422: If invalid parameters passed
+
+    """
     request_data = await prepare_request_data(request)
 
     for delivery_info in delivery_cache.values():
