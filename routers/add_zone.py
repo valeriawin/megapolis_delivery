5b58bdf3e71091a191ec0d016126d6669957b7cc
valeriawin <baranova.valeriya.by@gmail.com>
2022-07-24 22:05:11 +0300

    docs: add docstring to add_zone function

--- routers/add_zone.py
+++ routers/add_zone.py
@@ -10,6 +10,35 @@ zone_cache = {}
 
 @router.post("/add_zone", response_model=Feature)
 async def add_zone(request: Request) -> Feature:
+    """ Args:
+            request: body should contain a geojson feature information as JSON
+                    {
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Polygon",
+                            "coordinates": [
+                                [
+                                    [13.38272, 52.46385],
+                                    [13.42786, 52.46385],
+                                    [13.42786, 52.48445],
+                                    [13.38272, 52.48445],
+                                    [13.38272, 52.46385]
+                                ]
+                            ]
+                        },
+                        "properties": {
+                            "zone_name": "south east"
+                        }
+                    }
+
+        Returns:
+            Delivery zone Feature
+
+        Raises:
+            HTTPException 400: If nothing passed
+            HTTPException 422: If invalid parameters passed
+
+    """
     request_data = await prepare_request_data(request)
 
     new_zone_name = request_data["properties"]["zone_name"]
