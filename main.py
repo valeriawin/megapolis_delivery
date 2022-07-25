c987185fa0c86ebfe54e9ea5317c3fc29be36fb1
valeriawin <baranova.valeriya.by@gmail.com>
2022-07-25 08:49:57 +0300

    fix: quick fix

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 
+from routers import add_zone, add_deliveryman, deliver
+
 app = FastAPI()
+
+app.include_router(add_zone.router)
+app.include_router(add_deliveryman.router)
+app.include_router(deliver.router)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
